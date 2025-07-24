87aee5070ff8b702a45bb9467f04b82543be33a3
usama-imtiaz-07 <osamaimtiaz677@gmail.com>
2025-07-24 11:18:46 +0100

    added logo in navbar + new caraousel component

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import "../css/Navbar.css"
 function NavBar() {
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
+            <Link to="/"><img src='/src/assets/catLogo.png' alt='Movie Logo'/></Link>
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
---
+++ frontend/src/components/TrailerCaraousel.jsx
@@ -0,0 +1,2 @@
+import {Swiper, SwiperSlide} from "swiper/react";
+import { EffectCoverflow, Navigation, Autoplay, Pagination } from "swiper/modules"; 
